8b0e6fb163e032baab9a1aaa384bd4d7daea9993
Lucas <lucas.ferreira@blueshift.com.br>
2023-12-19 10:15:04 -0300

    cshtml

---
+++ myfinance-web-netcore/Controllers/PlanoContaController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace myfinance_web_netcore.Controllers
+{
+    [Route("[controller]")]
+    public class PlanoContaController : Controller
+    {
+        private readonly ILogger<PlanoContaController> _logger;
+
+        public PlanoContaController(ILogger<PlanoContaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
